73b370e26e1e9a5787ab28f901f41495103cb924
jahnavvivi <jahnavi.sharma1210@gmail.com>
2026-06-23 22:17:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# 🌐 Portfolio Website
+
+A personal portfolio website showcasing my projects, technical skills, achievements, and experience as a Computer Science student and developer.
+
+
+## 🛠️ Tech Stack
+
+- React.js
+- Vite
+- Tailwind CSS
+- JavaScript
+
+
+## 📬 Contact
+
+Feel free to connect with me for collaboration, internships, or project discussions.
+
+---
+
+Made with ❤️ by **Jahnavi Sharma**
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import dayjs from 'dayjs';
+import { navIcons, navLinks } from '#/constants'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt="Logo" />
+            <p className="font-bold">Jahnavi's Portfolio</p>
+            <ul>
+                {navLinks.map(({id, name})=>(
+                    <li key = {id}>{name}</li>
+                ))}
+
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map(({id,img}) =>(
+                    <li key={id}>
+                        <img src={img} className="icon-hover" alt="{`icon-${id}`}" />
+                    </li>
+                ))}
+            </ul>
+            <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

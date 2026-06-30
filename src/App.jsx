0b08be4b3aed020621d481a683969e240c42259d
jahnavvivi <jahnavi.sharma1210@gmail.com>
2026-06-30 10:52:16 +0530

    added hoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Dock, Navbar, Welcome } from './components'
 import gsap from 'gsap';
-
 import {Draggable} from 'gsap/Draggable';
 gsap.registerPlugin(Draggable);
+
+import { Dock, Navbar, Welcome } from './components'
+import { Terminal } from './windows';
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       <Navbar/>
       <Welcome/>
       <Dock/>
+
+      <Terminal/>
     </main>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
 import Dock from "./Dock";
+import WindowControls from './WindowControls';
 
 export {
-    Navbar, Welcome, Dock
+    Navbar, Welcome, Dock, WindowControls
 };
